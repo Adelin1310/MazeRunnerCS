ba0418ea159271182fad115652a88cc7e50b3ec0
Andrica Adelin <xadelin@yahoo.com>
2021-12-20 01:01:40 +0200

    Add project files.

---
+++ MazeRunnerCS/FereastraJoc.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MazeRunnerCS
+{
+    partial class FereastraJoc
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FereastraJoc));
+            this.labirint = new System.Windows.Forms.PictureBox();
+            this.caracter = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.labirint)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caracter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labirint
+            // 
+            this.labirint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.labirint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labirint.Image = ((System.Drawing.Image)(resources.GetObject("labirint.Image")));
+            this.labirint.Location = new System.Drawing.Point(0, 0);
+            this.labirint.Name = "labirint";
+            this.labirint.Size = new System.Drawing.Size(800, 450);
+            this.labirint.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.labirint.TabIndex = 0;
+            this.labirint.TabStop = false;
+            this.labirint.SizeChanged += new System.EventHandler(this.FereastraJoc_Resize);
+            this.labirint.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FereastraJoc_MouseMove);
+            this.labirint.Resize += new System.EventHandler(this.labirint_Resize);
+            // 
+            // caracter
+            // 
+            this.caracter.BackColor = System.Drawing.Color.Transparent;
+            this.caracter.Location = new System.Drawing.Point(407, 425);
+            this.caracter.Name = "caracter";
+            this.caracter.Size = new System.Drawing.Size(29, 25);
+            this.caracter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.caracter.TabIndex = 1;
+            this.caracter.TabStop = false;
+            this.caracter.Click += new System.EventHandler(this.caracter_Click);
+            this.caracter.MouseMove += new System.Windows.Forms.MouseEventHandler(this.caracter_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            // 
+            // FereastraJoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.caracter);
+            this.Controls.Add(this.labirint);
+            this.Name = "FereastraJoc";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.FereastraJoc_SizeChanged);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FereastraJoc_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.labirint)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caracter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox labirint;
+        private System.Windows.Forms.PictureBox caracter;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MazeRunnerCS/FereastraJoc.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeRunnerCS
+{
+    public partial class FereastraJoc : Form
+    {
+        private int _sizeX;
+        private int _sizeY;
+        private float _ratioX = 1.00f;
+        private float _ratioY = 1.00f;
+        private int gameDifficulty = 5;
+        /* 5 - Hard
+         * 0 - Normal
+         * -5 - Easy
+         */
+
+        private bool gameStart = false;
+        private int margineInterioaraWidth;
+        private int margineInterioaraHeight;
+        private Point punctZero;
+
+        private int xLatura2;
+        private int xLatura1;
+        private int yLatura2;
+        private int yLatura1;
+
+
+
+
+        public FereastraJoc()
+        {
+            InitializeComponent();
+            labirint.Image = Image.FromFile("F:\\Proiecte\\MazeRunnerCS\\MazeRunnerCS\\Resources\\labirint.png");
+            caracter.Image = Image.FromFile("F:\\Proiecte\\MazeRunnerCS\\MazeRunnerCS\\Resources\\caracter.png");
+            caracter.Location = new Point(caracter.Location.X, labirint.Size.Height - caracter.Size.Height);
+            System.Diagnostics.Debug.WriteLine(caracter.Location.ToString());
+            _sizeX = labirint.Size.Width;
+            _sizeY = labirint.Size.Height;
+            caracter.Size = new Size(caracter.Size.Width + gameDifficulty , caracter.Size.Height + gameDifficulty);
+            label1.Text = "Click on the character to start!";
+
+            margineInterioaraWidth = caracter.Size.Width - 4;
+            margineInterioaraHeight = caracter.Size.Height - 4;
+
+            xLatura2 = caracter.Location.X + margineInterioaraWidth;
+            xLatura1 = caracter.Location.X + 2;
+
+            yLatura2 = caracter.Location.Y + margineInterioaraHeight + 2;
+            yLatura1 = caracter.Location.Y + 2;
+
+            int pZeroX = caracter.Location.X + margineInterioaraWidth / 2 ;
+            int pZeroY = caracter.Location.Y + margineInterioaraHeight / 2 ;
+
+
+            punctZero = new Point(pZeroX, pZeroY);
+
+            label1.Text += $"{punctZero.ToString()}\n" +
+                $"Dreptunghi intern:\n" +
+                $"XLatura1: {xLatura1} XLatura2: {xLatura2}\n" +
+                $"YLatura1: {yLatura1} YLatura2: {yLatura2}";
+
+
+        }
+
+        private void FereastraJoc_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (gameStart)
+            {
+
+            }
+        }
+
+        private void FereastraJoc_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void caracter_Click(object sender, EventArgs e)
+        {
+            gameStart = true;
+            label1.Text = "Game started!";
+        }
+
+        private void labirint_Resize(object sender, EventArgs e)
+        {
+            //caracter.Location = new Point(caracter.Location.X + (labirint.Size.Width - _sizeX), caracter.Location.Y + (labirint.Size.Height - _sizeY));
+            //label1.Text += $"\nLabirint: {labirint.Size.Width} x {labirint.Size.Height}\n" +
+            //    $"Caracter: {caracter.Location.ToString()}";
+        }
+
+        private void FereastraJoc_SizeChanged(object sender, EventArgs e)
+        {
+        //    caracter.Location = new Point(caracter.Location.X + (this.Width - _sizeX), caracter.Location.Y + (this.Height - _sizeY));
+        //    label1.Text += $"\nLabirint: {this.Width} x {this.Height}";
+        //    this.
+        }
+
+        private void caracter_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (gameStart)
+            {
+                if(e.Location.X < xLatura1)
+                {
+                    caracter.Location = new Point(caracter.Location.X - (caracter.Location.X - e.X), caracter.Location.Y);
+                    xLatura2 = caracter.Location.X + caracter.Size.Width - 2;
+                    xLatura1 = caracter.Location.X + 2;
+                }
+                else if(e.Location.X > xLatura1)
+                {
+                    caracter.Location = new Point(caracter.Location.X + (caracter.Location.X + caracter.Size.Width - e.X), caracter.Location.Y);
+                    xLatura2 = caracter.Location.X + caracter.Size.Width - 2;
+                    xLatura1 = caracter.Location.X + 2;
+                }
+                if(e.Location.Y > yLatura2)
+                {
+                    caracter.Location = new Point(caracter.Location.X, caracter.Location.Y + (caracter.Location.Y + caracter.Size.Height - e.Y));
+                    yLatura2 = caracter.Location.Y + caracter.Size.Height - 2;
+                    yLatura1 = caracter.Location.Y + 2;
+                }
+                else if (e.Location.Y < yLatura1)
+                {
+                    caracter.Location = new Point(caracter.Location.X, caracter.Location.Y - (caracter.Location.Y + caracter.Size.Height - e.Y));
+                    yLatura2 = caracter.Location.Y + caracter.Size.Height - 2;
+                    yLatura1 = caracter.Location.Y + 2;
+                }
+            }
+        }
+    }
+}
+ 
